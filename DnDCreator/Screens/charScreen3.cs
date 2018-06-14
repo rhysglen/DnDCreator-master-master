e71bdff8a84a3765781ccdb7355081c1c31d83b8
temp <temp>
2018-06-14 13:33:22 -0400

    Add project files.

---
+++ DnDCreator/Screens/charScreen3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnDCreator
+{
+    public partial class charScreen3 : UserControl
+    {
+        public charScreen3()
+        {
+            InitializeComponent();
+
+            if (athButton.Checked)
+            {
+                Character.skills.Add("Athletics");
+            }
+            if (acroButton.Checked)
+            {
+                Character.skills.Add("Acrobatics");
+            }
+            if (handButton.Checked)
+            {
+                Character.skills.Add("Sleight of Hand");
+            }
+            if (stealthButton.Checked)
+            {
+                Character.skills.Add("Stealth");
+            }
+            if (arcaButton.Checked)
+            {
+                Character.skills.Add("Arcana");
+            }
+            if (hisButton.Checked)
+            {
+                Character.skills.Add("History");
+            }
+            if (inveButton.Checked)
+            {
+                Character.skills.Add("Investigation");
+            }
+            if (natButton.Checked)
+            {
+                Character.skills.Add("Nature");
+            }
+            if (religButton.Checked)
+            {
+                Character.skills.Add("Religion");
+            }
+            if (aniButton.Checked)
+            {
+                Character.skills.Add("Animal Handling");
+            }
+            if (medButton.Checked)
+            {
+                Character.skills.Add("Medicine");
+            }
+            if (percButton.Checked)
+            {
+                Character.skills.Add("Perception");
+            }
+            if (survButton.Checked)
+            {
+                Character.skills.Add("Survival");
+            }
+            if (decButton.Checked)
+            {
+                Character.skills.Add("Deception");
+            }
+            if (intimButton.Checked)
+            {
+                Character.skills.Add("Intimidation");
+            }
+            if (perfButton.Checked)
+            {
+                Character.skills.Add("Performance");
+            }
+            if (persuaButton.Checked)
+            {
+                Character.skills.Add("Persuasion");
+            }
+
+        }
+        
+    }
+    
+}
